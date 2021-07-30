e239072c1d2867c7c3c06831704587d44ef557e7
Lucas Henrique de Oliveira Matos <lucasmatos@pactosolucoes.com.br>
2021-07-30 13:19:50 -0300

    Novo usuário

---
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-usuario',
+  template: `
+    <p>
+      novo-usuario works!
+    </p>
+  `,
+  styleUrls: ['./novo-usuario.component.css']
+})
+export class NovoUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
